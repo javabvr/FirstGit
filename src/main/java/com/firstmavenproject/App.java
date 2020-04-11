d882806cf2064379096a0e8e63da85a918492953
javabvr <javabvr@gmail.com>
2020-04-11 13:15:14 +0530

    04/11 - Comment to just commit the changes

--- src/main/java/com/firstmavenproject/App.java
+++ src/main/java/com/firstmavenproject/App.java
@@ -1,6 +1,7 @@
 package com.firstmavenproject;
 
 public class App {
+	//Comment to just commit the changes 
 	public String returnSomething() {
 		System.out.println("From App method ");
 		return "---First Maven Project---";
